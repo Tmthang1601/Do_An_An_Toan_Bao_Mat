9754fc44f67a110b5df86994dddd1233fc7896b6
TMTHANG03 <21130537@st.hcmuaf.edu.vn>
2024-12-21 17:39:20 +0700

    thêm getByUserId() và insert() trong KeyService và css cho button Báo cáo private Key

--- src/main/java/com/bookshopweb/service/KeyService.java
+++ src/main/java/com/bookshopweb/service/KeyService.java
@@ -3,13 +3,28 @@ package com.bookshopweb.service;
 import com.bookshopweb.beans.Key;
 import com.bookshopweb.dao.KeyDao;
 import com.bookshopweb.dao.UserDAO;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class KeyService extends Service<Key, KeyDao> implements KeyDao {
+    public Optional<Key> getByUserId(long userId) {
+        try (Handle handle = Jdbi.create("your_jdbc_connection_url", "username", "password").open()) {
+            KeyDao keyDao = handle.attach(KeyDao.class);
+            return keyDao.getByUserId(userId);
+        }
+    }
     public KeyService() {
         super(KeyDao.class);
+    }
+
+    public long insert(Key key) {
+        try (Handle handle = Jdbi.create("your_jdbc_connection_url", "username", "password").open()) {
+            KeyDao keyDao = handle.attach(KeyDao.class);
+            return keyDao.insert(key);
+        }
     }
 
     @Override
